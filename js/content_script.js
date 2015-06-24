f55680da8634129359ac3e11783ef80fa3349c7b
knightingal <yujianming1985@126.com>
2015-06-24 23:34:45 +0800

    add Task object  Changes to be committed: 	modified:   content_script.js

--- js/content_script.js
+++ js/content_script.js
@@ -2,40 +2,14 @@
 //document.getElementById("lst-ib").value="test";
 function doTask() {
 //console.log(x[2])
-    console.log(window.location.href)
-    var jsonObj = {};
-    jsonObj.href = window.location.href
-    var t=document.getElementsByTagName("font");
-    //console.log(t);
-    //console.log(t[8].innerHTML);
-    var next_a = t[8].parentNode;
-    //console.log(next_a.href);
-    var title=t[4].innerHTML;
-    var titleString = new String(title);
-    //var titleShort = new String(titleString.slice(0, -4));
-    var titleShort = getTitleShort(titleString);
-
-    var next_title = getTitleShort(t[8].innerHTML);
-    jsonObj.title = titleShort;
-    var jsonArray = [];
-    var x=document.getElementsByTagName("p")
-    console.log(x)
-    for (i=2; i<x.length; i++) {
-        console.log(x[i].firstChild.src)
-        //jsonArray[i-2]=x[i].firstChild.src
-        jsonArray[i-2]=x[i].firstChild.src
-    }
-    jsonObj.list=jsonArray;
-    //console.log(x[4].innerHTML)
-    console.log(titleShort);
-    console.log(jsonObj)
-    console.log(jsonObj.toJSONString())
-
-    console.log(next_title.localeCompare(titleShort));
     
 
-    chrome.extension
-        .sendMessage(jsonObj.toJSONString(), 
+    Task.init();
+    var pageInfo = Task.getPageInfo();
+    
+
+    chrome.extension.sendMessage(
+        pageInfo.toJSONString(), 
         function(response) {
             if (next_title.localeCompare(titleShort) == 0) {
                 window.location.href = next_a;
@@ -49,6 +23,60 @@ function doTask() {
         }
     );
         
+}
+
+var Task = {
+    "fontElementArray": [],
+    "pElementArray": [],
+    "init": function() {
+        this.fontElementArray = document.getElementsByTagName("font");
+        this.pElementArray = document.getElementsByTagName("p");
+    },
+    "getCurrentTitle": function() {
+        return this.getTitleShort(
+                new String(this.fontElementArray[4].innerHTML)
+        );
+    },
+    "getNextTitle": function() {
+        return this.getTitleShort(
+                new String(this.fontElementArray[this.fontElementArray.length - 13].innerHTML)
+        );
+    },
+
+    "getNextUrl": function() {
+        return this.fontElementArray[this.fontElementArray.length - 13].parentNode.href;
+    },
+
+    "getTitleShort": function(titleString) {
+        return new String(titleString.slice(0, -4));
+    },
+
+    "getPageInfo": function() {
+        var imgSrcArray = [];
+        var pageInfoObj = {};
+        for (var i = 2; i < this.pElementArray.length; i++) {
+            imgSrcArray[i-2] = this.pElementArray[i].firstChild.src;
+        }
+        pageInfoObj.imgSrcArray = imgSrcArray;
+        pageInfoObj.title = this.getCurrentTitle();
+        return pageInfoObj;
+    },
+
+    "isLastPage": function() {
+        return this.getNextTitle().localeCompare(this.getCurrentTitle());
+    },
+
+    "doTask": function() {
+        var getInfo = this.getPageInfo();
+        console.log(getInfo);
+        self.port.emit("sendPageInfo", getInfo);
+        if (this.isLastPage() == 0) {
+            self.port.emit("goToNextPage", this.getNextUrl());
+        }
+        else {
+            self.port.emit("stopAndSendPageInfosToServer");
+        }
+    }
 }
 
 
