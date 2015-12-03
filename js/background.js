26498e47ef3df827513c3f3f625be24667ea0edc
knightingal <yujianming1985@126.com>
2015-12-04 00:15:34 +0800

    update for navy

--- js/background.js
+++ js/background.js
@@ -2,45 +2,19 @@ var isAutoRun = false;
 
 var totalRequest = {};
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    
+
     if (request === "checkIsAutoRun") {
         sendResponse(isAutoRun);
-    // } else if (request === "stop" ){
-    //     isAutoRun = false;
-    //     xmlhttp = new XMLHttpRequest()
-    //     console.log(totalRequest);
-    //     xmlhttp.open("POST", "http://127.0.0.1:8000/local1000/urls1000/", true)
-    //     //xmlhttp.open("POST", "http://127.0.0.1:8081/startDownload/", true)
-    //     xmlhttp.setRequestHeader("Content-Type", "application/json");
-    //     xmlhttp.onreadystatechange = function() {
-    //         if (xmlhttp.readyState==4) {
-    //             if (xmlhttp.status==200) {
-    //                 console.log("http://127.0.0.1:8081/startDownload/ return " 
-    //                     + xmlhttp.responseText)
-    //             }
-    //         }
-    //     }
-    //     xmlhttp.send(JSON.stringify(totalRequest));
-    //     totalRequest = {};
     } else {
-        
-        // var pageInfo = JSON.parse(request);
-        // isAutoRun = true;
-        // if (totalRequest["title"] === undefined) {
-        //     totalRequest["title"] = pageInfo["title"];
-        //     totalRequest["imgSrcArray"] = pageInfo["imgSrcArray"];
-        // } else if (totalRequest["title"] === pageInfo["title"]) {
-        //     totalRequest["imgSrcArray"] = totalRequest["imgSrcArray"].concat(pageInfo["imgSrcArray"]);
-        // }
         var xmlhttp = new XMLHttpRequest()
         // console.log(totalRequest);
-        xmlhttp.open("POST", "http://127.0.0.1:8000/local1000/navy/", true)
-        //xmlhttp.open("POST", "http://127.0.0.1:8081/startDownload/", true)
+        // xmlhttp.open("POST", "http://127.0.0.1:8000/local1000/navy/", true)
+        xmlhttp.open("POST", "http://127.0.0.1:8081/startDownload/donwLoadNavy/", true)
         xmlhttp.setRequestHeader("Content-Type", "application/json");
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState==4) {
                 if (xmlhttp.status==200) {
-                    console.log("http://127.0.0.1:8000/local1000/navy/ return " 
+                    console.log("http://127.0.0.1:8000/local1000/navy/ return "
                         + xmlhttp.responseText)
                 }
             }
@@ -49,15 +23,16 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
         totalRequest = {};
         sendResponse(null);
     }
-    
+
 });
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(null,
-        {
+    chrome.tabs.executeScript(null, {
             code: "doTask()"
-        }, 
-        function() {console.log("callback");}
+        },
+        function() {
+            console.log("callback");
+        }
 
     );
-    
+
 });
--- js/content_script.js
+++ js/content_script.js
@@ -1,16 +1,11 @@
 function doTask() {
     Task.init();
-    var pageInfo = Task.getPageInfo();
+    var pageInfo = Task.getPageInfo().toJSONString();
+    console.log(pageInfo);
     chrome.extension.sendMessage(
-        pageInfo.toJSONString(), 
+        pageInfo,
         function(response) {
             this.imgArrya = [];
-            // if (Task.isLastPage()) {
-            //     window.location.href = Task.getNextUrl();
-            // } else {
-            //     chrome.extension.sendMessage("stop", function(response) {});
-            // }
-            
         }
     );
 }
@@ -18,6 +13,19 @@ function doTask() {
 var Task = {
     "trElementArray": [],
     "pElementArray": [],
+    "pageInfoObjExample": {"imgArray":[
+        {
+          "imrSrc":"http://www.navsource.org/archives/01/0136005.jpg",
+          "description":"<i>\"Proposed B.S. #36\".</i><br>\nPreliminary design plan prepared for the General Board during consideration of designs for Battleship # 36, which became the <b><i>Nevada </i>(BB-36 / 37)</b> class. This plan, dated 4 March 1911, provides eight 14-inch guns, twin-screw reciprocating machinery and a speed of 21 knots in a ship 605 feet long on the load water line (L.W.L.), 95 feet in beam, with a normal displacement of 27,000 tons.\nThe original plan is in the 1911-1925 \"Spring Styles Book\".\n",
+          "copyright":"U.S. Naval Historical Center Photograph # S-584-001. "
+        },
+        {
+          "imrSrc":"http://navsource.org/archives/01/pdf/013600p.pdf",
+          "description":"<font size=\"3\"><b>Girl to Name New War Dog Will Be Sponsor for <b><i>Nevada </i>(BB-36)</b></b><br> Miss Eleanor Anne Siebert, a little miss of 10 years,has been named sponsor for the <b><i>Nevada</i></b>, the newest battleship of the United States navy, which will be launched in October. </font>",
+          "copyright":"<font size=\"3\">Image and text provided by University of California, Riverside.<br>Photo from <i>The San Francisco Call.</i>(San Francisco [Calif.]) 1895-1913, 17 August  1913, Image 9, via chroniclingamerica.loc.gov.  </font>"
+        },],
+        "title":"BB-36 USS NEVADA",
+      },
     "pageInfoObj": {},
     "imgArray": [],
     "init": function() {
@@ -26,21 +34,22 @@ var Task = {
         var j = 0;
         var notUseArray = [];
         for (var i = 0; i < this.trElementArray.length; i++) {
-           
+
             var tds = this.trElementArray[i].getElementsByTagName("td");
             if (tds.length >= 4) {
                 console.log(tds[0].childNodes[0].href + " " + j);
-                console.log(tds[2].innerHTML + " " + j);
-                console.log(tds[3].innerHTML + " " + j);
+                // console.log(tds[2].innerHTML + " " + j);
+                // console.log(tds[3].innerHTML + " " + j);
                 this.imgArray.push({
-                    "imrSrc": tds[0].childNodes[0].href, 
-                    "description": tds[2].innerHTML, 
-                    "copyright": tds[3].innerHTML})
+                    "imrSrc": tds[0].childNodes[0].href,
+                    // "description": tds[2].innerHTML,
+                    // "copyright": tds[3].innerHTML
+                  })
                 j++;
             } else {
                 notUseArray.push(this.trElementArray[i]);
             }
-            
+
         }
         this.pageInfoObj.imgArray = this.imgArray;
         var h1 = document.getElementsByTagName("h1")[1];
